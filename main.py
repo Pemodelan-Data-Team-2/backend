7bc6f58b050fe17c3b08179ff25cbf7383430834
unknown <23522048@std.stei.itb.ac.id>
2022-12-20 18:08:07 +0800

    change return type to dict instead of list

--- main.py
+++ main.py
@@ -22,28 +22,28 @@ def get_pa_counts_by_cc():
     """
     Function to get formatted data for Statistics page in frontend
     """
-    return create_reports.patient_admission_count_by_care_center()
+    return {'statistics': create_reports.patient_admission_count_by_care_center()}
 
 @app.route('/statistics/pa-counts-by-room-type', methods=['GET'])
 def get_pa_counts_by_room_type():
     """
     Function to get formatted data for Statistics page in frontend
     """
-    return create_reports.patient_admisson_count_by_room_type()
+    return {'statistics': create_reports.patient_admisson_count_by_room_type()}
 
 @app.route('/data/carecenters', methods=['GET'])
 def get_carecenters():
     """
     Function to get formatted data for Data page in frontend
     """
-    return create_reports.care_centers_table()
+    return {'data': create_reports.care_centers_table()}
 
 @app.route('/data/patient-admissions-by-country', methods=['GET'])
 def get_patient_admissions_by_country():
     """
     Function to get formatted data for Data page in frontend
     """
-    return create_reports.patient_admissions_table()
+    return {'data': create_reports.patient_admissions_table()}
 
 if __name__ == '__main__':
     app.run(debug=True)
