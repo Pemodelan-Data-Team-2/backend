0cf78eac4646d7871cfddaddeafe204ac340d58b
unknown <23522048@std.stei.itb.ac.id>
2022-12-22 09:14:50 +0800

    add headers

--- main.py
+++ main.py
@@ -10,47 +10,59 @@ def get_dashboard_data():
     """
     Function to get dictionary of all formatted data for Dashboard page in frontend
     """
-    return {
+    response = {
         'annualTotalAdmittedPatientsByRoomType': create_reports.annual_admitted_patients_by_room_type(),
         'annualTotalAdmittedPatientsByCountry': create_reports.annual_admitted_patients_bar_chart_data(),
         'annualRevenuesGeneratedFromPatientAdmissions': create_reports.annual_revenues_line_chart_data(),
         'roomsCountByCareCenter': create_reports.rooms_count_by_care_center(),
     }
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/statistics/pa-counts-by-carecenter', methods=['GET'])
 def get_pa_counts_by_cc():
     """
     Function to get formatted data for Statistics page in frontend
     """
-    return {'statistics': create_reports.patient_admission_count_by_care_center()}
+    response = {'statistics': create_reports.patient_admission_count_by_care_center()}
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/statistics/pa-counts-by-room-type', methods=['GET'])
 def get_pa_counts_by_room_type():
     """
     Function to get formatted data for Statistics page in frontend
     """
-    return {'statistics': create_reports.patient_admisson_count_by_room_type()}
+    response = {'statistics': create_reports.patient_admisson_count_by_room_type()}
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/statistics/beds-availability', methods=['GET'])
 def get_beds_availability_by_country():
     """
     Function to get formatted data for Statistics page in frontend
     """
-    return {'statistics': create_reports.beds_availability_by_current_date_table_data()}
+    response = {'statistics': create_reports.beds_availability_by_current_date_table_data()}
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/data/carecenters', methods=['GET'])
 def get_carecenters():
     """
     Function to get formatted data for Data page in frontend
     """
-    return {'data': create_reports.care_centers_table()}
+    response = {'data': create_reports.care_centers_table()}
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/data/patient-admissions-by-country', methods=['GET'])
 def get_patient_admissions_by_country():
     """
     Function to get formatted data for Data page in frontend
     """
-    return {'data': create_reports.patient_admissions_table()}
+    response = {'data': create_reports.patient_admissions_table()}
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
